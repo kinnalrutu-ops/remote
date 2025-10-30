efce745a14ae213c6dbfd534d32d0aaa93c8b500
Nagaveni.Barker <nagavenibarker6@gmail.com>
2025-10-30 15:17:18 +0530

    Save to teacher

---
+++ teacher.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the teacher name:"))
+b=int(input("Enter the teacher's subject:"))
+c=int(input("Enter the teacher's course code:"))
+print(f"Teacher name:{a}")
+print(f"Teacher's subject:{b}")
+print(f"Teacher's course code:{c}")
+ 
